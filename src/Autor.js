23c6e086a7985a594e00ba6196da02fb31c2e454
Gustavo Santiago <gustavo.santiago@fortaleza.ce.gov.br>
2017-10-27 11:44:17 -0300

    first commit

---
+++ src/Autor.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import PubSub from 'pubsub-js';
+
+export class FormularioAutor extends Component{
+  constructor() {
+    super();
+    this.state = {lista : [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  enviaForm(evento) {
+    evento.preventDefault();
+    
+    $.ajax({
+      url: 'http://cdc-react.herokuapp.com/api/autores',
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({
+        nome: this.state.nome, 
+        email: this.state.email, 
+        senha: this.state.senha
+      }),
+      success: function(response){
+        PubSub.publish('atualiza-lista-autores', response);
+      }, 
+      error: function(error){
+        console.log("erro");
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome: evento.target.value})
+  }
+
+  setEmail(evento){
+    this.setState({email: evento.target.value})
+  }
+
+  setSenha(evento){
+    this.setState({senha: evento.target.value})
+  }
+
+  render() {
+    return(
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+          <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+          <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+          <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                   <div className="pure-control-group">                                  
+            <label></label>
+            <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+          </div>
+        </form>
+      </div>
+    );
+  } 
+}
+
+class TabelaAutores extends Component{
+
+  render(){
+    return(
+      <div className="content" id="content">
+        <div>            
+          <table className="pure-table">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>email</th>
+              </tr>
+            </thead>
+            <tbody>
+              { 
+                this.props.lista.map(function(autor){
+                  return(
+                    <tr key={autor.id}>
+                      <td>{autor.nome}</td>
+                      <td>{autor.email}</td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table> 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class AutorBox extends Component {
+  constructor() {
+    super();
+    this.state = {lista : []};
+  }
+  
+  componentDidMount(){
+    $.ajax({
+      url: 'http://cdc-react.herokuapp.com/api/autores',
+      dataType: 'json',
+      success:function(resposta){    
+        this.setState({lista:resposta});
+      }.bind(this)
+    });
+
+    PubSub.subscribe('atualiza-lista-autores', function(topico, novaLista){
+      this.setState({lista:novaLista});
+    });
+  }
+  
+  render(){
+    return(
+      <div>
+        <FormularioAutor/>
+        <TabelaAutores lista={this.state.lista}/>
+      </div>
+    );
+  }
+}
